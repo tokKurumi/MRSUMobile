52e853db273b1dfb7f7efb4a891a7cf321656044
Oleg <tkurumi901@gmail.com>
2023-09-07 01:05:06 +0300

    Add project files.

---
+++ MRSUMobile/MRSUMobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MRSUMobile.ViewModels;
+using MRSUMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MRSUMobile
+{
+	public partial class AppShell : Xamarin.Forms.Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+			Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+			Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+		}
+
+		private async void OnMenuItemClicked(object sender, EventArgs e)
+		{
+			await Shell.Current.GoToAsync("//LoginPage");
+		}
+	}
+}
---
+++ MRSUMobile/MRSUMobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MRSUMobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MRSUMobile.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		public ItemDetailPage()
+		{
+			InitializeComponent();
+			BindingContext = new ItemDetailViewModel();
+		}
+	}
+}
